e5e7c7cf025f3ceef5fe233f95db1d5c7382c23b
kiran4399 <kiran4399@gmail.com>
2016-10-02 01:15:38 +0530

    ros driver updated

--- ros-node/src/beagleus_node.cpp
+++ ros-node/src/beagleus_node.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-namespace hc_sr04_node {
 
 // Maximum distance reported. Values over this distance
 // report MAX_DISTANCE. TODO make this a property.
@@ -18,46 +17,75 @@ const static float TRAVEL_TIME_MAX = MAX_DISTANCE * DIST_SCALE;
 int main(int argc, char **argv) {
 
   // Start ROS node.
+  string 
   ROS_INFO("Starting node");
-  ros::init(argc, argv, "hc_sr04s");
+  ros::init(argc, argv, "sonar rosnode");
   ros::NodeHandle node;
-  ros::Rate rate(800);  // 800 hz for 4 sensors: 200Hz
+  ros::Rate rate(200);  // 200 Hz
 
-  // TODO: config these
-  vector<hc_sr04_node::Sonar> sonars;
-  sonars.push_back(hc_sr04_node::Sonar(24, 25));
-  sonars.push_back(hc_sr04_node::Sonar(22, 23));
 
   // Build a publisher for each sonar.
-  vector<ros::Publisher> sonar_pubs;
-  for (int i = 0; i < sonars.size(); ++i) {
-    stringstream ss;
-    ss << "sonar_" << i;
-    sonar_pubs.push_back(node.advertise<sensor_msgs::Range>(ss.str(), 10));
-  }
+  vector<ros::Publisher> sonar_x;
+  vector<ros::Publisher> sonar_y;
+
+  ros::Publisher sonarx_pub = node.advertise<sensor_msgs::Range>("sonar_x", 10);
+  ros::Publisher sonary_pub = node.advertise<sensor_msgs::Range>("sonar_y", 10);
+
   
   // Build base range message that will be used for
   // each time a msg is published.
   sensor_msgs::Range range;
-  range.radiation_type = sensor_msgs::Range::ULTRASOUND;
-  range.min_range = 0.0;
-  range.max_range = hc_sr04_node::MAX_DISTANCE;
  
   float distance;
   bool error;
-  while(ros::ok()) {    
-    for (int i = 0; i < sonars.size(); ++i) {
+  while(ros::ok()) { 
+
+      ifstream sonar_left ("/dev//dev/rpmsg_pru30");
+      sonar_left >> string_left;
       range.header.stamp = ros::Time::now();
-      range.range = sonars[i].distance(&error);
+      range.range = distance;
       if (error)
-	       ROS_WARN("Error on sonar %d", i);
+	       ROS_WARN("Error on /us/sonar_x for the left sensor");
 
       else{
-	       sonar_pubs[i].publish(range);
+	       sonarx_pub.publish(range);
     }
-  }
+
+
+      range.header.stamp = ros::Time::now();
+      range.range = distance+200
+      if (error)
+         ROS_WARN("Error on /us/sonar_x for the right sensor");
+
+      else{
+         sonarx_pub.publish(range);
+    }
+
+
+      ifstream sonar_right ("/dev//dev/rpmsg_pru31");
+      sonar_right >> string_right;
+      range.header.stamp = ros::Time::now();
+      range.range = distance+400
+      if (error)
+         ROS_WARN("Error on /us/sonar_y for the front sensor");
+
+      else{
+         sonary_pub.publish(range);
+    }
+
+      range.header.stamp = ros::Time::now();
+      range.range = distance+600
+      if (error)
+         ROS_WARN("Error on /us/sonar_y for the back sensor");
+
+      else{
+         sonary_pub.publish(range);
+    }
+
+
     ros::spinOnce();
     rate.sleep();    
   }
+  
   return 0;
 }
